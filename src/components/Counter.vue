40d3bd3f93007f4d096891ab101de77c09d2069b
Daniel Schmidt <daniel.schmidt@comma-soft.com>
2023-01-21 11:43:31 +0100

    counter im localStore

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import { useCounterStore } from '../stores/counter';
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../stores/counter";
+import { useLocalStorage } from "@vueuse/core";
 
 const { count } = storeToRefs(useCounterStore());
-const store = useCounterStore();
+useLocalStorage("counter", count);
 
+const store = useCounterStore();
 </script>
 <template>
-Counter: {{ count }}
-<button @click="store.increment">+</button>
-
+  Counter: {{ count }}
+  <button @click="store.increment">+</button>
 </template>
